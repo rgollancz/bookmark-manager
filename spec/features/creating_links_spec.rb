e9e1cac4a388e80329a3c6c9c346dcf2b5c1a9d7
Rosie <r.gollancz@gmail.com>
2016-10-19 12:08:11 +0100

    Adds DatabaseCleaner to spec helper

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -4,9 +4,9 @@ require './app/models/link.rb'
 feature "adding links" do
   scenario "I would like to add sites address and a title" do
     visit'/links/new'
-    fill_in('url', with: 'www.makersacademy.com')
-    fill_in('title', with: 'Makers Academy')
+    fill_in('url', with: 'www.cats.com')
+    fill_in('title', with: 'Cats cats cats')
     click_button('Submit')
-    expect(page).to have_content('Makers Academy')
+    expect(page).to have_content('Cats cats cats')
   end
 end
